7cb6186fa50b1f55a24fc2b841fb2f2e6660c1a3
Leymar <buenaventuraleymar901@gmail.com>
2024-10-15 13:59:51 -0500

    se modifico algunos nombres de los metodo de la clase notificacion

--- src/models/notificacion.py
+++ src/models/notificacion.py
@@ -1,23 +1,16 @@
-from aplicacion import Aplicacion
-from celular import Celular
-from correo import Correo
+class Notificaciones:
+    def __init__(self, paciente, celular, correo):
+        self.cedula = ""
+        self.mensaje = ""
 
+    def notificar_agendamieto_de_cita(self):
+        pass
 
-class Notificacion:
-    """
-    Pide datos al metodo "cargar_datos" de la clase Hospital y genera un
-    mensaje dependiendo de que tipo de nofificación se va a realizar;
-    Notificación de creaciones de citas, notificacion de reagendamiento
-    de citas o notificacon sobre eliminiacion de una citas.
-    """
+    def notificar_cancelacion_de_cita(self):
+        pass
 
-    def __init__(self):
-        self.correo = Correo()
-        self.celular = Celular()
-        self.aplicacion = Aplicacion()
+    def notificar_reagendamiento_de_cita():
+        pass
 
-    def enviar_notificacion(self):
-        return self.aplicacion.enviar_notificacion()
-
-    def verificar_correo(self):
-        print(f"Verificando correo {self.correo}")
+    def enviar_recordatorio_de_cita():
+        pass
