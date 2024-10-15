34c0a7bc576f3435bc0637d8ae06146765f8337e
Leymar <buenaventuraleymar901@gmail.com>
2024-10-15 13:54:38 -0500

    se creo el metodo guardar datos csv falta crear su codigo

--- src/models/gestion_de_datos.py
+++ src/models/gestion_de_datos.py
@@ -1,46 +1,26 @@
 import os
 import json
-import csv
+import pandas as pd
 
 
 class GestionDeDatos:
     def __init__(self):
         base_dir = os.path.join(os.getcwd(), "src", "data")
-        self.ruta_datos_medicos = os.path.join(base_dir, "medicos.json")
-        self.ruta_datos_pacientes = os.path.join(base_dir, "pacientes.csv")
-        self.ruta_datos_citas = os.path.join(base_dir, "citas.csv")
+        self.ruta_datos_json_medicos = os.path.join(base_dir, "medicos.json")
 
-        self.datos_medicos = {}
-        self.datos_pacientes = []
-        self.datos_citas = []
+        # Cargar datos al inicializar la clase
+        self.datos_medicos = self.cargar_datos_json_medicos()
+        self.datos_pacientes = pd.read_csv(os.path(base_dir, 'pacientes.csv'))
+        self.datos_citas = pd.read_csv(os.path(base_dir, 'citas.csv'))
 
-    def cargar_datos_medicos(self):
-        with open(self.ruta_datos_medicos, 'r', encoding='utf-8') as file:
+    def cargar_datos_json_medicos(self):
+        with open(self.ruta_datos_json_medicos, 'r', encoding='utf-8') as file:
             return json.load(file)
 
-    def cargar_datos_pacientes(self):
-
-        with open(self.ruta_datos_pacientes, 'r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            return list(reader)
-
-    def cargar_datos_citas(self):
-
-        with open(self.ruta_datos_citas, 'r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            return list(reader)
-
-    def actualizar_datos_json(self):
-
+    def actualizar_datos_json(self, nuevos_datos):
         # Guardar datos de médicos en JSON
-        with open(self.ruta_datos_medicos, 'w', encoding='utf-8') as file:
-            json.dump(self.datos_medicos, file, ensure_ascii=False, indent=4)
+        with open(self.ruta_datos_json_medicos, 'w', encoding='utf-8') as file:
+            json.dump(nuevos_datos, file, ensure_ascii=False, indent=4)
 
     def actualizar_datos_csv(self):
-        # Guardar datos de pacientes en CSV
-        with open(self.ruta_datos_pacientes, 'w', encoding='utf-8', newline='') as file:
-            fieldnames = self.datos_pacientes[0].keys(
-            ) if self.datos_pacientes else []
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(self.datos_pacientes)
+        pass
