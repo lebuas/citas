60ab432a1daf71823fa8f1ebad73b11f5b7452fe
Leymar <buenaventuraleymar901@gmail.com>
2024-10-15 15:45:18 -0500

    se crearon variables para almacenar la ruta de los archivos

--- src/models/gestion_de_datos.py
+++ src/models/gestion_de_datos.py
@@ -5,22 +5,36 @@ import pandas as pd
 
 class GestionDeDatos:
     def __init__(self):
+        # Base directory for data files
         base_dir = os.path.join(os.getcwd(), "src", "data")
-        self.ruta_datos_json_medicos = os.path.join(base_dir, "medicos.json")
 
-        # Cargar datos al inicializar la clase
+        # Paths to files
+        self.ruta_datos_json_medicos = os.path.join(base_dir, "medicos.json")
+        self.ruta_datos_pacientes = os.path.join(base_dir, 'pacientes.csv')
+        self.ruta_datos_citas = os.path.join(base_dir, 'citas.csv')
+
+        # Load data when the class is initialized
         self.datos_medicos = self.cargar_datos_json_medicos()
-        self.datos_pacientes = pd.read_csv(os.path(base_dir, 'pacientes.csv'))
-        self.datos_citas = pd.read_csv(os.path(base_dir, 'citas.csv'))
+        self.datos_pacientes = pd.read_csv(self.ruta_datos_pacientes)
+        self.datos_citas = pd.read_csv(self.ruta_datos_citas)
 
     def cargar_datos_json_medicos(self):
-        with open(self.ruta_datos_json_medicos, 'r', encoding='utf-8') as file:
-            return json.load(file)
+        # Load JSON data for doctors
+        if os.path.exists(self.ruta_datos_json_medicos):
+            with open(self.ruta_datos_json_medicos, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        else:
+            print(f"Archivo {self.ruta_datos_json_medicos} no encontrado.")
+            return {}
 
     def actualizar_datos_json(self, nuevos_datos):
-        # Guardar datos de médicos en JSON
+        # Save updated doctors data to JSON
         with open(self.ruta_datos_json_medicos, 'w', encoding='utf-8') as file:
             json.dump(nuevos_datos, file, ensure_ascii=False, indent=4)
 
-    def actualizar_datos_csv(self):
-        pass
+    def actualizar_datos_csv(self, tipo, datos):
+        # Save updated data to CSV files
+        if tipo == 'pacientes':
+            datos.to_csv(self.ruta_datos_pacientes, index=False)
+        elif tipo == 'citas':
+            datos.to_csv(self.ruta_datos_citas, index=False)
